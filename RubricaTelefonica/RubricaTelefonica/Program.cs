ac48ec83fe569f388f588db83cac240d451aa9f0
Francesco Valentini <Francesco.valentini930@gmail.com>
2024-10-17 12:50:30 +0200

    Metodi ausiliari per la CLI

--- RubricaTelefonica/RubricaTelefonica/Program.cs
+++ RubricaTelefonica/RubricaTelefonica/Program.cs
@@ -1,7 +1,37 @@
-﻿namespace RubricaTelefonica {
+﻿using RubricaTelefonica.Models.Models;
+
+namespace RubricaTelefonica {
     public class RubricaTelefonica {
+        private static Rubrica rubr;
         public static void Main(String[] args) {
-            Console.WriteLine("Hello World");
+            if(rubr == null) {
+                rubr = new Rubrica(MenuPrimoAvvio());
+            }
+        }
+
+        private static string MenuPrimoAvvio() {
+            return ReadString("Specificare il nome della rubrica:");
+        }
+
+        private static string Menu() {
+            Console.WriteLine("A) Aggiungi contatto");
+            Console.WriteLine("R) Ricerca contatto");
+            Console.WriteLine("E) Elimina contatto");
+            Console.WriteLine("S) Stampa rubrica");
+            Console.WriteLine("");
+
+            return ReadString(rubr.nome + "> ");
+        }
+
+
+        private static string ReadString(string prompt) {
+            string s = "";
+            do {
+                Console.Write(prompt);
+                s = Console.ReadLine();
+            } while (s != "");
+
+            return s;
         }
 
     }
