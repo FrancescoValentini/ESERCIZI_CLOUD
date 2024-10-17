83006b83ac43cef45ab1281d5bdfb91e2bf2fb2a
Francesco Valentini <Francesco.valentini930@gmail.com>
2024-10-17 15:07:11 +0200

    Correzione al main del server

--- ClientServerExample/Server/Program.cs
+++ ClientServerExample/Server/Program.cs
@@ -2,7 +2,7 @@
 
 namespace Server {
     class Server {
-        public static void main(String[] args) {
+        public static void Main(String[] args) {
             TCPServer srv = new TCPServer("127.0.0.1", 2323);
             srv.StartServer();
         }
