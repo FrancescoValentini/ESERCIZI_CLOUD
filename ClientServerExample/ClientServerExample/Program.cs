afa8653f8d28841682ea41587564f9fef98432bd
Francesco Valentini <Francesco.valentini930@gmail.com>
2024-10-17 14:57:44 +0200

    Correzione errore classi invertite

--- ClientServerExample/ClientServerExample/Program.cs
+++ ClientServerExample/ClientServerExample/Program.cs
@@ -4,8 +4,7 @@ namespace ClientServerExample {
     class ClientServerExample {
 
         public static void Main(String[] args) {
-            TCPServer srv = new TCPServer("127.0.0.1", 2323);
-            srv.StartServer();
+
         }
     }
 }
--- ClientServerExample/Server/Program.cs
+++ ClientServerExample/Server/Program.cs
@@ -1,7 +1,10 @@
-﻿namespace Server {
+﻿using ClientServerExampleModels.Models;
+
+namespace Server {
     class Server {
         public static void main(String[] args) {
-            Console.WriteLine("Hello World");
+            TCPServer srv = new TCPServer("127.0.0.1", 2323);
+            srv.StartServer();
         }
     }
 }
