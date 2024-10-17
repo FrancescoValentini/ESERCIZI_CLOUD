d87b2fbad0089d2a0988029e51423d9f6cef6960
Francesco Valentini <Francesco.valentini930@gmail.com>
2024-10-17 15:06:29 +0200

    Main per il client TCP

--- ClientServerExample/ClientServerExample/Program.cs
+++ ClientServerExample/ClientServerExample/Program.cs
@@ -4,7 +4,26 @@ namespace ClientServerExample {
     class ClientServerExample {
 
         public static void Main(String[] args) {
+            TCPClient client = SetupClient();
 
+            client.Connect();
+            while (client.IsClientConnected()) {
+                Console.Write("> ");
+                string datiDaInviare = Console.ReadLine();
+                client.Send(datiDaInviare);
+                string rx = client.Receive();
+                Console.WriteLine("Ricevuto: {0}",rx);
+            }
+    
+
+        }
+
+        public static TCPClient SetupClient() {
+            Console.Write("IP: ");
+            string ip = Console.ReadLine();
+            Console.Write("Port: ");
+            Int32 port = Int32.Parse(Console.ReadLine());
+            return new TCPClient(ip, port);
         }
     }
 }
