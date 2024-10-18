c47370a356a7410c8761c98da5d74f843a133180
Francesco Valentini <Francesco.valentini930@gmail.com>
2024-10-18 12:11:38 +0200

    Test del programma

--- TassiDiCambio/TassiDiCambio/Program.cs
+++ TassiDiCambio/TassiDiCambio/Program.cs
@@ -1,7 +1,45 @@
-﻿namespace TassiDiCambio {
+﻿
+
+using TassiDiCambioModels.Models;
+using TassiDiCambioModels.Models.ClassiServizioRest;
+
+namespace TassiDiCambio
+{
     public class TassiDiCambio {
-        public static void Main(String[] args) {
-            Console.WriteLine("Hello, World!");
+        public static void Main(string[] args) {
+            // URL dell'API della Banca d'Italia (simulato in questo esempio)
+            string url = "http://127.0.0.1/tassi.json";
+
+            // Inizializza l'oggetto per gestire i tassi di cambio
+            BANKITALIATassiDiCambio tassiCambio = new BANKITALIATassiDiCambio(url);
+            tassiCambio.DoRESTrequest();
+            // Ottieni i tassi di cambio
+            TassiCambioData dati = tassiCambio.GetTassiCambio();
+
+            // Stampa alcune informazioni di esempio
+            Console.WriteLine($"Totale record: {dati.ResultsInfo.TotalRecords}");
+            Console.WriteLine($"Prima valuta: {dati.Currencies[0].Name}, Paese: {dati.Currencies[0].Countries[0].StrCountry}");
+
+            Console.WriteLine("ISO CODE ADP");
+            Currency c = tassiCambio.GetCurrencyByIsoCode("ADP");
+            Console.WriteLine(c.Name);
+
+            Console.WriteLine("COUNTRY NAME ANDORRA");
+            Currency c1 = tassiCambio.GetCurrencyByCountryName("ANDORRA");
+            Console.WriteLine(c1.Name);
+
+            Console.WriteLine("Tutti gli iso");
+            List<string> a = tassiCambio.GetAllCurrenciesIso();
+            foreach (string code in a) {
+                Console.WriteLine($"{code}");
+            }
+
+
+            Console.WriteLine("Tutti Country");
+            List<string> a1 = tassiCambio.GetAllCountriesNames();
+            foreach (string code in a1) {
+                Console.WriteLine($"{code}");
+            }
         }
     }
 }
