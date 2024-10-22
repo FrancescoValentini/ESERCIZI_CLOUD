6829d67173db358f38c31e78e6ff20aa455f81db
Francesco Valentini <Francesco.valentini930@gmail.com>
2024-10-22 10:45:35 +0200

    Classi per gestire la rubrica

---
+++ WinForm_RUBRICA/WinFormRubrica_models/Models/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormRubrica_models.Models {
+    public class Persona {
+        public string nome { get; set; }
+        public string cognome { get; set; }
+        public string telefono { get; set; }
+
+        public Persona(string nome, string cognome, string telefono) {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.telefono = telefono;
+        }
+
+        public override string ToString() {
+            return "Nome: " + this.nome + "\nCognome: " + this.cognome + "\nTelefono: " + this.telefono;
+        }
+    }
+
+}
