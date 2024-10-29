9f82e6e20c90aa70cbcb2e71a793ff6c17d15bb1
Francesco Valentini <Francesco.valentini930@gmail.com>
2024-10-29 18:57:33 +0100

    inizializzazione progetto mvc rubrica

---
+++ MVC-Example1/MVC-Example1/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace MVC_Example1.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
