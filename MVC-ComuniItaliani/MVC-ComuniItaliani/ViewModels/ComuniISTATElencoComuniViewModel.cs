157eca3c354bf11b1822215d53b8b2ff9db619a9
Francesco Valentini <Francesco.valentini930@gmail.com>
2024-11-12 12:23:37 +0100

    View Model

---
+++ MVC-ComuniItaliani/MVC-ComuniItaliani/ViewModels/ComuniISTATElencoComuniViewModel.cs
@@ -0,0 +1,6 @@
+﻿using FunzioniISTAT.Modelli;
+namespace MVC_ComuniItaliani.ViewModels {
+    public class ComuniISTATElencoComuniViewModel {
+        public Comune[] ElencoComuni { get; set; }
+    }
+}
