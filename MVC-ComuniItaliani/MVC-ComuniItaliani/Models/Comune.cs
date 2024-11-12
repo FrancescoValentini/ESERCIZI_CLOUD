48acf21bf41730bd7794466240dd2ea0145fc861
Francesco Valentini <Francesco.valentini930@gmail.com>
2024-11-12 12:24:58 +0100

    Rimossi elementi inutili

--- MVC-ComuniItaliani/MVC-ComuniItaliani/Models/Comune.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MVC_ComuniItaliani.Models {
-	public class Comune {
-		public string CodiceCatastaleComune { get; set; }
-		public string DenominazioneItaliano { get; set; }
-		public string RipartizioneGeografica { get; set; }
-		public string DenominazioneRegione { get; set; }
-		public string DenominazioneUnitaSovracomunale { get; set; }
-	}
-}
