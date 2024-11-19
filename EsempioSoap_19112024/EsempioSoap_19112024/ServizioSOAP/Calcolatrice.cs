a4193dcf65078b30628421dfd4f3b5a3b548153b
Francesco Valentini <Francesco.valentini930@gmail.com>
2024-11-19 11:26:09 +0100

    Update Calcolatrice.cs

--- EsempioSoap_19112024/EsempioSoap_19112024/ServizioSOAP/Calcolatrice.cs
+++ EsempioSoap_19112024/EsempioSoap_19112024/ServizioSOAP/Calcolatrice.cs
@@ -1,6 +1,11 @@
-﻿namespace EsempioSoap_19112024.ServizioSOAP {
+﻿using System.ServiceModel;
+
+namespace EsempioSoap_19112024.ServizioSOAP {
+    [ServiceContract]
     public interface I_Calcolatrice {
+        [OperationContract]
         public int Somma(int a, int b);
+        [OperationContract]
         public int Sottrazione(int a, int b);
     }
     public class Calcolatrice : I_Calcolatrice {
