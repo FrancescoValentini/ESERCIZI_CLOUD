ac6473edce8574ad0b747931a26e8e5e7578bf76
Francesco Valentini <Francesco.valentini930@gmail.com>
2024-11-19 12:37:00 +0100

    Servizio REST locali storici Bologna

---
+++ TestProgettoApi/TestProgettoApi/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Bologna.Modelli;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestProgettoApi.Controllers {
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+
+   
+    public class TestController : ControllerBase {
+        [HttpGet]
+        public int somma(int a, int b) {
+            return a + b;
+        }
+        [HttpGet]
+        public LocaleBologna[] getLocali() {
+            return ServiziBologna.GetLocaliAsync().Result;
+        }
+
+        [HttpGet]
+        public LocaleBologna[] ricercaLocali(String nome) {
+            return ServiziBologna.RicercaLocali(nome).Result;
+        }
+    }
+}
