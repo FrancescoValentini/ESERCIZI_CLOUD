753153128ab2026915252f9b016272957bc56dbf
Francesco Valentini <Francesco.valentini930@gmail.com>
2024-11-19 11:24:29 +0100

    Classe servizio SOAP (calcolatrice)

---
+++ EsempioSoap_19112024/EsempioSoap_19112024/ServizioSOAP/Calcolatrice.cs
@@ -0,0 +1,15 @@
+﻿namespace EsempioSoap_19112024.ServizioSOAP {
+    public interface I_Calcolatrice {
+        public int Somma(int a, int b);
+        public int Sottrazione(int a, int b);
+    }
+    public class Calcolatrice : I_Calcolatrice {
+        public int Somma(int a, int b) {
+            return a + b;
+        }
+
+        public int Sottrazione(int a, int b) {
+            return a - b;
+        }
+    }
+}
